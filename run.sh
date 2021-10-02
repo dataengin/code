2a4162f8a8432ace6c137cdd4eb70d7a5b33fb7c
Pluto-fox <github.fek0e@slmail.me>
2021-10-02 19:16:15 +0000

    Revert "Bash"

--- run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-argo_tunnel(){
-cloudflared --hostname vs.clfd.ml --url http://0.0.0.0:$PORT
-}
-argo_tunnel &
-chmod 777 /run/screen
-/usr/local/bin/code-server --disable-telemetry --bind-addr 0.0.0.0:$PORT /home/coder/cdr
